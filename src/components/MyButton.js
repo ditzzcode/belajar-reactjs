723ff79fac9369b72e826fddeb8548711cc88b9b
ditzzcode <radityahuuu@gmail.com>
2024-12-14 20:38:41 +0800

    mempelajari useState

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,9 +1,32 @@
+import { useState } from "react"
+
 export default function MyButton(){
-  const clicked = () => {
-    return alert("button ini di klick")
+const [getValue = 0, setValue] = useState(0)
+const [getWord , setWord] = useState("")
+
+  const plus = () => {
+    setValue(getValue + 1)
+    if(getValue >= 19) {
+      setWord("button ini sudah di klick sebanyak " + getValue)
+    }
   }
 
+  const minus = () => {
+    setValue(getValue - 1)
+    if(getValue <= 0) {
+      alert("angka tidak bisa negatif")
+      setValue(0)
+    }
+  }
+
+
+
   return (
-    <button onClick={() => clicked()}>click me bro</button>
+    <div>
+      <div>{getWord}</div>
+      <div>{getValue}</div>
+      <button onClick={() => plus()}>plus</button>
+      <button onClick={() => minus()}>minus</button>
+    </div>
   )
 }
