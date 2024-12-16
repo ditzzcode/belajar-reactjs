aa7a2e7589d9fbfe85ebe2fda46c51bc642adca7
ditzzcode <radityahuuu@gmail.com>
2024-12-16 20:23:28 +0800

    belajar useMemo-1 16/12/2024

--- src/UseMemo/ExpensiveComponent.js
+++ src/UseMemo/ExpensiveComponent.js
@@ -1,23 +1,23 @@
 import { useState , useMemo } from "react";
 
-function ExpensiveComponent() {
+function ExpensiveComponents(){
   const [count , setCount] = useState(0);
-  const [number , setNumber] = useState(1);
+  const [number ,setNumber] = useState(1);
 
-  const expensiveCalculation = (num) => {
-    console.log("calculating. . .");
-    return num * 2;
-  };
+  const expensiveCalculate = (num) => {
+    console.log("procesing...")
+    return num * 2
+  }
 
-  const result = useMemo(() => expensiveCalculation(number) , [number]);
+  const result = useMemo(() => expensiveCalculate(number) , [number]);
 
   return (
     <div>
-      <h2>count {count}</h2>
-      <h2>Hasil : {result}</h2>
-      <button onClick={() => setCount(count + 1)}>tambah count</button>
-      <button onClick={() => setNumber(number + 1)}>tambah number</button>
+      <h2>result : {result}</h2>
+      <button onClick={() => setCount(count + 1)}>tombol count</button>
+      <button onClick={() => setNumber(number + 1)}>tombol result</button>
     </div>
   )
 }
-export default ExpensiveComponent;
+
+export default ExpensiveComponents
